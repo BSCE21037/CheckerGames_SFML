4c8c289667dd112e578656dacd2c43012de5e55f
BSCE21037 <bsce21037@itu.edu.pk>
2023-08-04 18:54:01 +0500

    changing to starting position

--- main.cpp
+++ main.cpp
@@ -3,143 +3,144 @@
 using namespace std;
 using namespace sf;
 
-// class Player{
-// private:
+const int boardSize = 8;	//size of board
+const int tileSize = 80;	//size of tiles
+int tileCheck = 0;			//check if tile is white or black
+int tileValue[8][8];			//value of tile
 
 
-// public:
+RectangleShape tile(sf::Vector2f(tileSize, tileSize));	//create tile   (global variable)
+CircleShape checkerPiece(40.f);                         //create checker piece  (global variable)
 
+void drawChessBoard(RenderWindow &window){              //draw chess board function
 
-// };
+	
+	bool isWhite = true;	//is tile white or black
 
-int size = 56; //size of board
-Sprite f[32]; //sprite array
-int board[8][8] = {-1,-2,-3,-4,-5,-3,-2,-1,
-                    -6,-6,-6,-6,-6,-6,-6,-6
-                    ,0,0,0,0,0,0,0,0
-                    ,0,0,0,0,0,0,0,0
-                    ,0,0,0,0,0,0,0,0
-                    ,0,0,0,0,0,0,0,0
-                    ,6,6,6,6,6,6,6,6
-                    ,1,2,3,4,5,3,2,1}; //board array
+	for(int i = 0; i < boardSize; i++){                 //for loop
+		for(int j = 0; j < boardSize; j++){             //nested for loop
+			tileValue[i][j] = -99;                      //set tile value to -99
+		}
+	}
+   
+    for (int i = 0; i < boardSize; ++i){		//draw board
+        for (int j = 0; j < boardSize; ++j){	//draw board
 
-
-void loadPosition(){
-    int k = 0;
-    for(int i = 0; i < 8; i++){//loop through board array
-        for(int j = 0; j < 8; j++){ //loop through board array
-            int n = board[i][j];        //set n to board array
-            if(!n) continue;            //if n is not 0 continue
-            int x = abs(n) - 1;         //set x to absolute value of n minus 1
-            int y = n > 0 ? 1 : 0;      //set y to n greater than  1 : 
-            f[k].setTextureRect(IntRect(56 * x, 56 * y, 56, 56));   //set texture rect
-            f[k].setPosition(56 * j, 56 * i);                       //set position
-                    //explana
-            // pieces[k].setTextureRect(IntRect(size * x, size * y, size, size));
-            // pieces[k].setPosition(size * j, size * i);
-            k++;
-
+            tile.setFillColor(isWhite ? sf::Color::White : sf::Color::Black);	//if white true then color white else color black
+            tile.setPosition(j * tileSize, i * tileSize);						//set position of tile within the row
+            window.draw(tile);													//draw tile
+			if(tile.getFillColor() == sf::Color::Black){                        //if tile is black
+				tileValue[i][j] = 0;                                            //set tile value to 0
+			}
+            isWhite = !isWhite;													//flip color
         }
-    }
+        isWhite = !isWhite;														//flip color before next row
+    }	
+}
+//--
+void drawCheckerPiece(RenderWindow &window){        //draw chess board function
+
+	bool isRed = true;	//is piece white or black
+	int player1Pieces = 12;     //number of pieces for player 1     
+	int player2Pieces = 12;     //number of pieces for player 2
+
+	int Player1Tiles = 3;       //number of rows of tiles for player 1
+	int Player2Tiles = 5;       //number of starting rows of tiles for player 2
+
+    for (int i = 0; i < boardSize; ++i){		//draw board
+        for (int j = 0; j < boardSize; ++j){	//draw board
+				if(i < Player1Tiles){                                    //if row is less than player 1 tiles
+
+					checkerPiece.setFillColor(isRed ? sf::Color::Red : sf::Color::Blue);	//if red true then color red else color blue
+					checkerPiece.setPosition(j * tileSize, i * tileSize);               //set position of piece within the row
+					if(tileValue[i][j] == 0){                                        //if tile value is 0
+						window.draw(checkerPiece);                                  //draw piece
+						tileValue[i][j] =  1;                                       //set tile value to 1
+					}
+					isRed = !isRed;													//flip color
+				}
+				else if(Player2Tiles <= i){
+					checkerPiece.setFillColor(isRed ? sf::Color::Blue : sf::Color::Red);	//if red true then color blue else color red
+					checkerPiece.setPosition(j * tileSize, i * tileSize);           //set position of piece within the row
+					if(tileValue[i][j] == 0){                                       //if tile value is 0
+						window.draw(checkerPiece);                                  //draw piece
+						tileValue[i][j] =  1;                                       //set tile value to 1
+					}
+					isRed = !isRed;                                                 //flip color
+				}
+		}
+		isRed = !isRed;														//flip color before next row	
+	}
 }
 
-int main()                //main function
+
+int main()
 {
-    RenderWindow window(sf::VideoMode(600, 600), "Checkers", Style::Close | Style::Titlebar);  //create window with size 600x600, title TicTacToe, and close and titlebar buttons on window 
-    window.setFramerateLimit(60);   //set framerate limit to 60
+    bool move = false;      //check if piece is moving
+    double dx = 0, dy = 0;  //change in x and y
 
-    Texture red,board;        //create textures
-
-    if(!red.loadFromFile("red.png")) //if texture is not loaded
-    {
-        cout << "Error loading texture" << endl; //print error message
-    }
-    if(!board.loadFromFile("chess_board.png")) //if texture is not loaded
-    {
-        cout << "Error loading texture" << endl; //print error message
-    }
-    
-    // red.setSmooth(true); //set texture to smooth
-
-
-    Sprite r;          //create sprite
-    
-    r.setTexture(red); //set texture to sprite
-    
-    r.setScale(0.04, 0.04); //set scale of sprite
-
-
-
-    Sprite b;          //create sprite
-    b.setTexture(board); //set texture to sprite
-    b.setScale(0.25, 0.25); //set scale of sprite
-
-    for(int i = 0; i < 32; i++)
-    {
-        f[i].setTexture(red); //set texture to sprite
-        f[i].setScale(0.04, 0.04); //set scale of sprite
-    }
-
-    loadPosition();
-
-    bool Move = false; //bool for move detection
-
-    float x = 0; //float for x position
-    float y = 0; //float for y position
+	sf::RenderWindow window(sf::VideoMode(tileSize * boardSize, tileSize * boardSize), "Checkers"); //create window
 
     while (window.isOpen()) //while window is open
     {
-
-        Vector2i mousePos = Mouse::getPosition(window); //get mouse position
-        Event event;        //create event
+        sf::Event event;    //create event
         while (window.pollEvent(event)) //while window is polling for event
         {
 			switch (event.type) //switch statement for event type
 			{
-                case Event::MouseButtonPressed: //if mouse button is pressed
-                    if(event.mouseButton.button == Mouse::Left)
-                    {
-                        if(r.getGlobalBounds().contains(mousePos.x, mousePos.y)) //if mouse position is inside sprite
-                        {
-                            Move = true; //set move to true
-                            x = mousePos.x - r.getPosition().x; //set x to mouse position minus sprite position
-                            y = mousePos.y - r.getPosition().y; //set y to mouse position minus sprite position
 
-                        }
-                        
-                    }
-                    break;
+			case Event::MouseButtonPressed:    //if key is pressed    
+                if(event.mouseButton.button == Mouse::Left){    //if left mouse button is pressed
+                    Vector2i mousePos = Mouse::getPosition(window); //get mouse position
+                    Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y)); //get mouse position
+                    cout << "mouse x: " << mousePos.x << endl;  //print mouse x position
+                    cout << "mouse y: " << mousePos.y << endl;  //print mouse y position
+					cout << "tile x: " << mousePos.x / tileSize << endl;    //print tile x position
+					cout << "tile y: " << mousePos.y / tileSize << endl;    //print tile y position
+					cout << "tile value: " << tileValue[mousePos.y / tileSize][mousePos.x / tileSize] << endl;  //print tile value
+					//write a code for the movement of the piece
+					if(tileValue[mousePos.y / tileSize][mousePos.x / tileSize] == 1){   //if tile value is 1
+						move = true;                                                //set move to true
+					}
+					else{                                                        //else
+						move = false;                                            //set move to false
+					}
+					//write a code to drag the piece    
+					if(move){                                                    //if move is true
+						dx = mousePos.x - tile.getPosition().x;                 //set dx to mouse x position - tile x position
+						dy = mousePos.y - tile.getPosition().y;                //set dy to mouse y position - tile y position
+					}
 
-                case Event::MouseButtonReleased: //if mouse button is released
-                    if(event.mouseButton.button == Mouse::Left)
-                    {
-                        Move = false; //set move to false
-                    }
-                    break;
+                    
+                }
+                break;        //break
+            
+            case Event::MouseButtonReleased:	//released
+				if (event.mouseButton.button == Mouse::Left)	//if left mouse button is released
+				{
+					move = false;   //set move to false
+				}
+				
+				break;          //break
 
-                case Event::KeyPressed: //if key is pressed
-                    if(event.key.code == Keyboard::Escape) //if key is escape
-                    {
-                        window.close(); //close window
-                    }
-                    break; //break
-
-			        case Event::Closed: //if window is closed
-                    window.close();     //close window
-                    break;                  //break
+			case Event::Closed: //if window is closed
+				window.close();     //close window
+				break;                  //break
 
 			}
-            if(Move) //if move is true
-            {
-                r.setPosition(mousePos.x - x, mousePos.y - y); //set sprite position to mouse position minus x and y
-            }
-
+			if(move){   //if move is true
+				checkerPiece.setPosition(Mouse::getPosition(window).x - dx, Mouse::getPosition(window).y - dy); //set position of piece to mouse position - dx and dy
+			}
+            
         }
-        window.clear(Color::White); //clear window with white color
-        window.draw(b);        //draw sprite
-        for(int i=0;i<32;i++){ window.draw(f[i]);       } //draw sprite
-        window.display();           //display window
+
+        window.clear(sf::Color::White); //clear window
+        drawChessBoard(window);        //draw chess board
+		drawCheckerPiece(window);   //draw checker piece
+        window.display();    //display window
     }
 
-    return 0;                   
+		window.display();       //display window
+	
+    return 0;
 } 
